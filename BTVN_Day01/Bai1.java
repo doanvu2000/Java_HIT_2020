99ce7d30c1ce5fccd48e3a9e677fac624ea41b0a
Doan Vũ <55150896+doanvu2000@users.noreply.github.com>
2020-09-25 17:29:10 +0700

    Update Bai1.java
    update

--- BTVN_Day01/Bai1.java
+++ BTVN_Day01/Bai1.java
@@ -4,15 +4,9 @@ public class Bai1 {
 		int m = 6, n = 11;
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= n; j++) {
-				if (i == 1 || i == m) {// Hàng đầu, hàng cuối
+				if (i == 1 || i == m || j == 1 || j == n) {
 					System.out.print("* ");
-				} else {
-					if (j == 1 || j == n) { // Các hàng ở giữa
-						System.out.print("* "); // Vị trí đầu và cuối hàng 
-					}else {
-						System.out.print("  ");//Giữa hàng
-					}
-				}
+				} else System.out.print("  ");
 			}
 			System.out.println();
 		}
